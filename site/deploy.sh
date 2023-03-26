58e6b5febfb7773b44ae1cfc72f9e1045f19f199
XiaoPan <2489964425@qq.com>
2023-03-26 14:27:22 +0800

    chore(file):清理dist文件

--- site/deploy.sh
+++ site/deploy.sh
@@ -9,7 +9,7 @@ pnpm run docs:build
 # 进入待发布的目录
 cd docs/.vitepress/dist
 
-git remote add prince_ui git@gitee.com:pan__xiaopan/prince_ui.git
+git remote add prince_ui https://github.com/easyestui/easyest.git
 git add -A
 git commit -m 'docs(update):更新文档'
 
@@ -17,6 +17,6 @@ git commit -m 'docs(update):更新文档'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f prince_ui main
+git push -f prince_ui master
 
 # cd -
