b9c1a9b0207209214a18105cbbcc721d990cf4c6
XiaoPan <2489964425@qq.com>
2023-03-26 13:39:12 +0800

    docs(update):更新文档

---
+++ site/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+# set -e
+
+# 构建
+pnpm run docs:build
+
+# 进入待发布的目录
+cd docs/.vitepress/dist
+
+git remote add prince_ui git@github.com:PrinceUI-core/prince_ui.git
+git add -A
+git commit -m 'docs(update):更新文档'
+
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+git push -f prince_ui master
+
+# cd -
