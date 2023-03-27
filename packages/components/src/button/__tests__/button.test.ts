2206b28d305de7ff978986c7f8f9394b011692c6
XiaoPan <2489964425@qq.com>
2023-03-27 12:25:45 +0800

    test(components):tests the type property of the button

--- packages/components/src/button/__tests__/button.test.ts
+++ packages/components/src/button/__tests__/button.test.ts
@@ -1,17 +1,25 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import { mount } from "@vue/test-utils";
 import button from "../button.vue";
 // The component to test
 describe("test button", () => {
-    it("should render slot", () => {
+    test("should render slot", () => {
         const wrapper = mount(button, {
             slots: {
                 default: "Prince_UI"
             }
         });
-
         // Assert the rendered text of the component
         expect(wrapper.text()).toContain("Prince_UI");
+    });
+
+    test("should render type", () => {
+        const wrapper = mount(button, {
+            props: {
+                type: "primary"
+            }
+        });
+        expect(wrapper.props().type).toStrictEqual("primary");
     });
 });
--- play/App.vue
+++ play/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <p-button type="primary">主要按钮</p-button>
+        <p-button>Not Type</p-button>
+        <p-button type="default">Default</p-button>
+        <p-button type="primary">Primary</p-button>
+        <p-button type="success">Success</p-button>
+        <p-button type="info">Info</p-button>
+        <p-button type="warning">Warning</p-button>
+        <p-button type="error">Error</p-button>
     </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" se tup></script>
