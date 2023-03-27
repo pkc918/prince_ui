abf57ee38f8f628352647eda38c1ea0b2ab1f036
XiaoPan <2489964425@qq.com>
2023-03-27 12:51:10 +0800

    fix(config):修复shell脚本

--- publish.sh
+++ publish.sh
@@ -4,11 +4,11 @@
 # set -e
 
 # 构建
-pnpm run build:prince_ui
+pnpm run build:prince_ui &&
 # 切换官方源
-pnpm config set registry https://registry.npmjs.org
+pnpm config set registry https://registry.npmjs.org &&
 # 版本发布
-pnpm run publish:prince_ui
+pnpm run publish:prince_ui &&
 # 切回淘宝源
 pnpm config set registry https://registry.npm.taobao.org
 
